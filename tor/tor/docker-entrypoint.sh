2f6ebe2d0f3ea8f742a4eb8e5259c164960951f7
chamyeongdo <chamyeongdo@icloud.com>
2024-07-23 22:41:01 +0900

    fix: http 프록시 서버

--- tor/tor/docker-entrypoint.sh
+++ tor/tor/docker-entrypoint.sh
@@ -5,6 +5,7 @@ for INSTANCE in $(seq 0 $((INSTANCES-1))); do
     cat > /etc/tor/torrc-${INSTANCE} << EOF
 DataDirectory /var/lib/tor-${INSTANCE}
 SocksPort 0.0.0.0:$((9050 + INSTANCE))
+HTTPTunnelPort 0.0.0.0:$((8050 + INSTANCE))
 
 CircuitBuildTimeout 5
 
