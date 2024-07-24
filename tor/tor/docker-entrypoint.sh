82ad164c02671a6beddc0dcf5b79e0bdddc19252
chamyeongdo <chamyeongdo@icloud.com>
2024-07-24 12:43:26 +0900

    fix: tor

--- tor/tor/docker-entrypoint.sh
+++ tor/tor/docker-entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 for INSTANCE in $(seq 0 $((INSTANCES-1))); do
     cat > /etc/tor/torrc-${INSTANCE} << EOF
 DataDirectory /var/lib/tor-${INSTANCE}
+
 SocksPort 0.0.0.0:$((9050 + INSTANCE))
 HTTPTunnelPort 0.0.0.0:$((8050 + INSTANCE))
 
@@ -13,9 +14,6 @@ StrictNodes 1
 
 NewCircuitPeriod 300
 MaxCircuitDirtiness 300
-
-StrictNodes 1
-ExitNodes ${EXIT_NODES}
 
 EOF
 done
